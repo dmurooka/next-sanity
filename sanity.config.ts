184196d92e91f4ba7061d4cb1065a6c3c4683a5e
dmurooka <dmurooka@gmail.com>
2023-04-29 16:51:53 +0900

    Set up sanity/next project

---
+++ sanity.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "sanity";
+import {deskTool } from 'sanity/desk';
+
+
+const config = defineConfig({
+    projectId: "0fwginzl",
+    dataset: "production",
+    title: "My Personal Website",
+    apiVersion:"2023-29-04",
+    basePath: "/admin",
+    plugins: [deskTool()]
+})
+
+export default config;
