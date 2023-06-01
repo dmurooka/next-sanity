388b5224756699f3edb4ddc86be3d3e8d5491697
dmurooka <dmurooka@gmail.com>
2023-06-01 14:50:32 +0900

    Styled layout

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default async function Home() {
   const projects = await getProjects();
 
   return (
-    <div className="max-w-5xl mx-auto py-20">
+    <div>
       <h1 className="text-7xl font-extrabold">
         Hello I'm{" "}
         <span className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent">
@@ -26,7 +26,7 @@ export default async function Home() {
           <Link
             href={`/projects/${project.slug}`}
             key={project._id}
-            className="border-2 border-gray-500 rounded-lg p-1 hover:scale-105 hover:border-blue-500 transition"
+            className="border-2 border-gray-500 rounded-lg p-1 hover:scale-105 transition"
           >
             {project.image && (
               <Image
--- app/projects/[project]/page.tsx
+++ app/projects/[project]/page.tsx
@@ -1,4 +1,6 @@
 import { getProject } from "@/sanity/sanity-utils";
+import { PortableText } from "@portabletext/react";
+import Image from "next/image";
 
 type Props = {
   params: { project: string };
@@ -9,7 +11,7 @@ export default async function Project({ params }: Props) {
   const project = await getProject(slug);
 
   return (
-    <div className="max-w-3xl mx-auto py-20">
+    <div>
       <header className="flex items-center justify-between">
         <h1 className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent text-5xl drop-shadow font-extrabold">
           {project.name}
@@ -25,6 +27,19 @@ export default async function Project({ params }: Props) {
           View Project
         </a>
       </header>
+
+      {/* content goes here*/}
+      <div className="text-lg text-gray-700 mt-5">
+        <PortableText value={project.content} />
+      </div>
+      {/* image goes here */}
+      <Image
+        src={project.image}
+        alt={project.name}
+        width={1920}
+        height={1080}
+        className="mt-10 border-2 border-grey-700 object-cover rounded-x1"
+      />
     </div>
   );
 }
