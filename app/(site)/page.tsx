6303dc0d6c3f231965ca4fa53228a3575ae83d4f
dmurooka <dmurooka@gmail.com>
2023-06-02 15:11:16 +0900

    Updated local packages

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -10,7 +10,7 @@ export default async function Home() {
       <h1 className="text-7xl font-extrabold">
         Hello I'm{" "}
         <span className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent">
-          Kapehe
+          Daishin
         </span>
         !
       </h1>
