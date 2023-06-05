18b4322d9d98cd4ee506065f615ad72825a108c0
dmurooka <dmurooka@gmail.com>
2023-06-05 22:43:53 +0900

    Styled top page

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -18,21 +18,24 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body className="max-w-3xl mx-auto py-10">
-        <header>
+        <header className="flex items-center justify-between">
           <Link
             href="/"
             className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent text-lg font-bold"
           >
             Daishin
           </Link>
-          <div>
+          <div className="flex items-center gap-5 text-sm text-gray-600">
             {pages.map((page) => (
-              <Link key={page._id} href={`/${page.slug}`}>
+              <Link
+                key={page._id}
+                href={`/${page.slug}`}
+                className="hover:underline"
+              >
                 {page.title}
               </Link>
             ))}
           </div>
-          ç
         </header>
         <main className="py-20">{children}</main>
       </body>
