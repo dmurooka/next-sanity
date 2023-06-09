f4b846295b44c37d91dc0e7dc6d47f7ff7bd8d3e
dmurooka <dmurooka@gmail.com>
2023-06-09 15:41:30 +0900

    Fixed horizontal margin for small screen

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -6,7 +6,7 @@ export default async function Home() {
   const projects = await getProjects();
 
   return (
-    <div className="mx-auto">
+    <div className="sm:mx-auto">
       <h1 className="text-7xl font-extrabold">
         Hello I&apos;m{" "}
         <span className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent">
