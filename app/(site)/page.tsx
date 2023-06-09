a9824a20625827800d07b7e13149ec15d94fc1bc
dmurooka <dmurooka@gmail.com>
2023-06-09 14:57:36 +0900

    Updated top page text

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -16,7 +16,8 @@ export default async function Home() {
       </h1>
 
       <p className="mt-3 text-xl text-gray-600">
-        Aloha everyone! Check out my projects!
+        Aloha everyone! Check out my projects! Ultimately, I am building a
+        community with love
       </p>
 
       <h2 className="mt-24 font-bold text-gray-700 text-3xl">My Projects</h2>
