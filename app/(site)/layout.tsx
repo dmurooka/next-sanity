11e44e36448d109c29e70a52439f8ddb2ce1cff8
dmurooka <dmurooka@gmail.com>
2023-06-09 17:41:34 +0900

    Fixed layout

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -16,7 +16,7 @@ export default async function RootLayout({
   const pages = await getPages();
 
   return (
-    <html lang="en" className="mx-5">
+    <html lang="en" className="mx-2">
       <body className="max-w-3xl mx-auto py-10">
         <header className="flex items-center justify-between">
           <Link
