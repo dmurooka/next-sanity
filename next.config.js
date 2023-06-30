832878de4b08571d9ff572203b1ae7ec134ae505
dmurooka <dmurooka@gmail.com>
2023-06-30 16:45:02 +0900

    Organized path

--- next.config.js
+++ next.config.js
@@ -7,12 +7,12 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'cdn.sanity.io',
-        port: '',
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        port: "",
       },
     ],
   },
-}
+};
 
 module.exports = nextConfig;
